1e8bfba6dd418d45d53fc751798e7b33e3b4bfb7
xristos2121 <xristo2121@gmail.com>
2024-06-05 22:35:35 +0300

    feat(search): updated to load search filters on result

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -7,49 +7,25 @@ use App\Http\Requests\AdvancedSearchRequest;
 use App\Models\Publication;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
 
 class SearchController extends Controller
 {
-    public function index()
+    public function index(): View
     {
         return view('front.advanced-search');
     }
 
-    public function search(AdvancedSearchRequest $request)
+    public function advanced_search(AdvancedSearchRequest $request): View
     {
-
         $query = Publication::query();
 
         foreach ($request->type as $key => $type) {
             $lookfor = $request->lookfor[$key];
-
             if (!empty($lookfor)) {
-                switch ($type) {
-                    case 'title':
-                        $query->orWhere('title', 'like', "%$lookfor%");
-                        break;
-                    case 'author':
-                        $query->orWhereHas('authors', function ($query) use ($lookfor) {
-                            $query->where(DB::raw("CONCAT(first_name, ' ', last_name)"), 'like', "%$lookfor%");
-                        });
-                        break;
-                    case 'abstract':
-                        $query->orWhere('abstract', 'like', "%$lookfor%");
-                        break;
-                        case 'keyword':
-                        $query->orWhereHas('keywords', function ($query) use ($lookfor) {
-                            $query->where('keyword', 'like', "%$lookfor%");
-                        });
-                        case 'publisher':
-                        $query->orWhereHas('publisher', function ($query) use ($lookfor) {
-                            $query->where('name', 'like', "%$lookfor%");
-                        });
-                    case 'entire_document':
-                        break;
-                }
+                $this->applyFilter($query, $type, $lookfor);
             }
         }
-
 
         if ($request->filled('fromMonthYear')) {
             $fromDate = Carbon::parse($request->fromMonthYear)->startOfMonth();
@@ -61,12 +37,94 @@ class SearchController extends Controller
             $query->where('publication_date', '<=', $untilDate);
         }
 
-
         $results = $query->get();
 
-
-        return view('front.results', compact('results'));
+        // Pass the search parameters to the view
+        return view('front.results', [
+            'results' => $results,
+            'searchParameters' => $request->all()
+        ]);
     }
 
 
+
+    private function applyFilter($query, $type, $lookfor)
+    {
+        switch ($type) {
+            case 'title':
+                $query->where('title', 'like', "%$lookfor%");
+                break;
+            case 'author':
+                $query->whereHas('authors', function ($query) use ($lookfor) {
+                    $query->where(DB::raw("CONCAT(first_name, ' ', last_name)"), 'like', "%$lookfor%");
+                });
+                break;
+            case 'abstract':
+                $query->where('abstract', 'like', "%$lookfor%");
+                break;
+            case 'keyword':
+                $query->whereHas('keywords', function ($query) use ($lookfor) {
+                    $query->where('keyword', 'like', "%$lookfor%");
+                });
+                break;
+            case 'publisher':
+                $query->whereHas('publisher', function ($query) use ($lookfor) {
+                    $query->where('name', 'like', "%$lookfor%");
+                });
+                break;
+            case 'entire_document':
+                $query->where(function ($subQuery) use ($lookfor) {
+                    $subQuery->where('title', 'like', "%$lookfor%")
+                        ->orWhere('abstract', 'like', "%$lookfor%")
+                        ->orWhereHas('authors', function ($query) use ($lookfor) {
+                            $query->where(DB::raw("CONCAT(first_name, ' ', last_name)"), 'like', "%$lookfor%");
+                        })
+                        ->orWhereHas('keywords', function ($query) use ($lookfor) {
+                            $query->where('keyword', 'like', "%$lookfor%");
+                        })
+                        ->orWhereHas('publisher', function ($query) use ($lookfor) {
+                            $query->where('name', 'like', "%$lookfor%");
+                        });
+                });
+                break;
+        }
+    }
+
+
+    public function search(Request $request): View
+    {
+        $query = Publication::query();
+
+        $searchTerm = $request->search;
+        $this->applySearchFilters($query, $searchTerm);
+
+        $results = $query->get();
+
+        $searchParameters = [
+            'type' => ['entire_document'],
+            'lookfor' => [$searchTerm]
+        ];
+
+
+        return view('front.results', [
+            'results' => $results,
+            'searchParameters' => $searchParameters
+        ]);
+
+    }
+
+    private function applySearchFilters($query, $searchTerm)
+    {
+        $query->where('title', 'like', "%$searchTerm%")
+            ->orWhere('abstract', 'like', "%$searchTerm%")
+            ->orWhereHas('authors', function ($query) use ($searchTerm) {
+                $query->where(DB::raw("CONCAT(first_name, ' ', last_name)"), 'like', "%$searchTerm%");
+            })
+            ->orWhereHas('keywords', function ($query) use ($searchTerm) {
+                $query->where('keyword', 'like', "%$searchTerm%");
+            })
+            ->orWhereHas('publisher', function ($query) use ($searchTerm) {
+                $query->where('name', 'like', "%$searchTerm%");
+            });
+    }
 }
--- resources/views/front/results.blade.php
+++ resources/views/front/results.blade.php
@@ -2,32 +2,63 @@
     <div class="container">
         <h1>Advanced Publication Search</h1>
         <form id="advanced-search-form" action="{{ url('/advanced-search/results') }}" method="GET" class="col-sm-8">
-            <div class="search-results-input-search">
-                <div class="search-results-input-search-wrapper">
-                    <div class="search-results-input-search-select-wrapper">
-                        <select name="type" style="border: none; background: none; margin-right: 10px;">
-                            <option value="entire_document" selected>Entire Document</option>
-                            <option value="title">Title</option>
-                            <option value="author">Author</option>
-                            <option value="abstract">Abstract</option>
-                            <option value="keyword">Keyword</option>
-                            <option value="publisher">Publisher</option>
-                        </select>
-                    </div>
-                    <input type="text" name="lookfor" class="form-control"
-                           style="flex: 1; border: none; outline: none;">
-                </div>
-                <div class="search-button">
-                    <button type="submit" style="background: none; border: none; padding: 0 10px; cursor: pointer;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                             class="feather feather-search">
-                            <circle cx="11" cy="11" r="8"></circle>
-                            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                        </svg>
-                    </button>
-                </div>
+            <div id="filters-container">
+                @if(isset($searchParameters['type']) && isset($searchParameters['lookfor']))
+                    @foreach($searchParameters['type'] as $index => $type)
+                        <div class="form-group filter-group d-flex align-items-center">
+                            <select name="type[]" class="form-control mr-2">
+                                <option value="entire_document" {{ $type === 'entire_document' ? 'selected' : '' }}>
+                                    Entire Document
+                                </option>
+                                <option value="title" {{ $type === 'title' ? 'selected' : '' }}>Title</option>
+                                <option value="author" {{ $type === 'author' ? 'selected' : '' }}>Author</option>
+                                <option value="abstract" {{ $type === 'abstract' ? 'selected' : '' }}>Abstract</option>
+                                <option value="keyword" {{ $type === 'keyword' ? 'selected' : '' }}>Keyword</option>
+                                <option value="publisher" {{ $type === 'publisher' ? 'selected' : '' }}>Publisher
+                                </option>
+                            </select>
+                            <input type="text" name="lookfor[]" class="form-control mr-2"
+                                   value="{{ $searchParameters['lookfor'][$index] ?? '' }}">
+                            @if($index > 0)
+                                <div type="button" onclick="removeFilter(this)">
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="20" height="20" viewBox="0 0 256 256" xml:space="preserve">
+                                        <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
+                                            <path d="M 11 90 c -2.815 0 -5.63 -1.074 -7.778 -3.222 c -4.295 -4.296 -4.295 -11.261 0 -15.557 l 68 -68 c 4.297 -4.296 11.26 -4.296 15.557 0 c 4.296 4.296 4.296 11.261 0 15.557 l -68 68 C 16.63 88.926 13.815 90 11 90 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(214,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"/>
+                                            <path d="M 79 90 c -2.815 0 -5.63 -1.074 -7.778 -3.222 l -68 -68 c -4.295 -4.296 -4.295 -11.261 0 -15.557 c 4.296 -4.296 11.261 -4.296 15.557 0 l 68 68 c 4.296 4.296 4.296 11.261 0 15.557 C 84.63 88.926 81.815 90 79 90 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(214,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"/>
+                                        </g>
+                                    </svg>
+                                </div>
+                            @endif
+                        </div>
+                    @endforeach
+                @endif
             </div>
+            <button type="button" class="addFilterBtn" onclick="addFilter()">Add Filter</button>
+            <div class="form-group">
+                <label for="fromMonthYear">From</label>
+                <input type="month" id="fromMonthYear" name="fromMonthYear" class="form-control"
+                       value="{{ $searchParameters['fromMonthYear'] ?? '' }}">
+            </div>
+            <div class="form-group">
+                <label for="untilMonthYear">Until</label>
+                <input type="month" id="untilMonthYear" name="untilMonthYear" class="form-control"
+                       value="{{ $searchParameters['untilMonthYear'] ?? '' }}">
+            </div>
+            <div class="form-group">
+                <label for="hits_per_page">Hits per page</label>
+                <select name="hits_per_page" class="form-control">
+                    <option value="10" {{ ($searchParameters['hits_per_page'] ?? '') == '10' ? 'selected' : '' }}>10
+                    </option>
+                    <option value="20" {{ ($searchParameters['hits_per_page'] ?? '') == '20' ? 'selected' : '' }}>20
+                    </option>
+                    <option value="50" {{ ($searchParameters['hits_per_page'] ?? '') == '50' ? 'selected' : '' }}>50
+                    </option>
+                    <option value="100" {{ ($searchParameters['hits_per_page'] ?? '') == '100' ? 'selected' : '' }}>
+                        100
+                    </option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Search</button>
         </form>
 
         <div class="search-results-section">
@@ -91,10 +122,10 @@
                                         <div class="publication-data-row">
                                             <span class="publication-data-label">File:</span>
                                             <span class="publication-data-value">
-                                          <a href="{{ Storage::url($result->file) }}" class="preview-publication"
-                                             target="_blank">
-                                                Preview Publication
-                                            </a>
+                                                <a href="{{ Storage::url($result->file) }}" class="preview-publication"
+                                                   target="_blank">
+                                                    Preview Publication
+                                                </a>
                                             </span>
                                         </div>
                                     @endif
@@ -111,4 +142,46 @@
                 @endif
             </div>
         </div>
+    </div>
+
+    <script>
+        function addFilter() {
+            const filterContainer = document.getElementById('filters-container');
+            const newFilter = document.createElement('div');
+            newFilter.className = 'form-group filter-group d-flex align-items-center';
+            newFilter.innerHTML = `
+                <select name="type[]" class="form-control mr-2">
+                    <option value="entire_document">Entire Document</option>
+                    <option value="title">Title</option>
+                    <option value="author">Author</option>
+                    <option value="abstract">Abstract</option>
+                    <option value="keyword">Keyword</option>
+                    <option value="publisher">Publisher</option>
+                </select>
+                <input type="text" name="lookfor[]" class="form-control mr-2">
+                <div onclick="removeFilter(this)">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                     version="1.1" width="20" height="20" viewBox="0 0 256 256" xml:space="preserve">
+                                    <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;"
+                                       transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
+                                        <path
+                                            d="M 11 90 c -2.815 0 -5.63 -1.074 -7.778 -3.222 c -4.295 -4.296 -4.295 -11.261 0 -15.557 l 68 -68 c 4.297 -4.296 11.26 -4.296 15.557 0 c 4.296 4.296 4.296 11.261 0 15.557 l -68 68 C 16.63 88.926 13.815 90 11 90 z"
+                                            style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(214,0,0); fill-rule: nonzero; opacity: 1;"
+                                            transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"/>
+                                        <path
+                                            d="M 79 90 c -2.815 0 -5.63 -1.074 -7.778 -3.222 l -68 -68 c -4.295 -4.296 -4.295 -11.261 0 -15.557 c 4.296 -4.296 11.261 -4.296 15.557 0 l 68 68 c 4.296 4.296 4.296 11.261 0 15.557 C 84.63 88.926 81.815 90 79 90 z"
+                                            style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(214,0,0); fill-rule: nonzero; opacity: 1;"
+                                            transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"/>
+                                    </g>
+                                </svg>
+                </div>
+            `;
+            filterContainer.appendChild(newFilter);
+        }
+
+        function removeFilter(button) {
+            const filterGroup = button.closest('.filter-group');
+            filterGroup.remove();
+        }
+    </script>
 </x-front-layout>
