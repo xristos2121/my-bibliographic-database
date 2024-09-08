cb382103e0a16d5767e9d9c7cc0f7e39a7b1636d
xristos2121 <xristo2121@gmail.com>
2024-09-08 13:43:25 +0300

    chore: dont run migrations and copy .env if already exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 # Ensure .env file exists and is writable
 if [ ! -f /var/www/.env ]; then
     cp /var/www/.env.example /var/www/.env
+    chown laravel:www-data /var/www/.env
+    chmod 664 /var/www/.env
 fi
-chown laravel:www-data /var/www/.env
-chmod 664 /var/www/.env
 
 # Run composer install as laravel user
 su -c "composer install --no-interaction --no-plugins --no-scripts" -s /bin/sh laravel
@@ -27,7 +27,9 @@ fi
 su -c "php artisan config:cache" -s /bin/sh laravel
 
 # Run database migrations
-su -c "php artisan migrate --force" -s /bin/sh laravel
+if [ ! "$(php artisan migrate:status | grep 'Yes')" ]; then
+    su -c "php artisan migrate --force" -s /bin/sh laravel
+fi
 
 # Start PHP-FPM
 exec "$@"
